b0d47046723ad5e5092b57d83efe4fc823470cc9
Angely Granados <angelinagranados@gmail.com>
2020-11-03 16:29:37 -0500

    intro content and slider

---
+++ src/components/Slider.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Carousel from "react-elastic-carousel";
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+  { width: 768, itemsToShow: 3 },
+];
+
+const Slider = ({ items }) => {
+  return (
+    <Carousel className="slider" breakPoints={breakPoints}>
+      {items.map((item) => (
+        <div key={item.id}>
+          <img src={item.image_url} />
+          <span>{item.title}</span>
+        </div>
+      ))}
+    </Carousel>
+  );
+};
+
+export default Slider;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import CardList from "../components/CardList";
+import Slider from "../components/Slider";
 
 const Home = ({ featured }) => {
   return (
@@ -8,10 +9,36 @@ const Home = ({ featured }) => {
       <section className="home__hero">
         <img src="https://cdn.pixabay.com/photo/2016/05/07/19/21/baguette-1378049_1280.jpg" />
       </section>
-      <section className="home__intro"></section>
-      <section className="home__featured_products">
+      <section className="home__intro wrapper">
+        <div className="intro__images">
+          <img
+            className="intro__images--first"
+            src="https://cdn.pixabay.com/photo/2016/11/29/08/24/bakery-1868396_960_720.jpg"
+            alt="Amasado de pan"
+          />
+          <img
+            className="intro__images--second"
+            src="https://cdn.pixabay.com/photo/2016/11/29/09/00/abundance-1868573_960_720.jpg"
+            alt="Vitrina de productos"
+          />
+        </div>
+        <div className="intro__content">
+          <h2>¡El mejor pan de la ciudad!</h2>
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum."
+          </p>
+        </div>
+      </section>
+      <section className="home__featured_products wrapper">
         <h2>Destacados</h2>
-        <CardList products={featured} />
+        <Slider items={featured} />
+        {/* <CardList products={featured} /> */}
       </section>
     </section>
   );
