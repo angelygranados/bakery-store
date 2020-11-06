22d0131a3fd5ebe7f5645e36314a8aedac2c704e
Angely Granados <angelinagranados@gmail.com>
2020-11-06 10:48:47 -0500

    shopping cart remove

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,6 +1,7 @@
 import React from "react";
+import trashIcon from "../assets/images/waste-64.png";
 
-const CartItem = ({ id, title, qtty, image_url, price }) => {
+const CartItem = ({ id, title, qtty, image_url, price, handleDelete }) => {
   return (
     <tr key={id}>
       <td>
@@ -9,6 +10,14 @@ const CartItem = ({ id, title, qtty, image_url, price }) => {
       <td>{title}</td>
       <td>{qtty}</td>
       <td>{Number(price) * qtty}</td>
+      <td>
+        <img
+          className="icon"
+          src={trashIcon}
+          alt="Eliminar producto"
+          onClick={(e) => handleDelete(e, id)}
+        />
+      </td>
     </tr>
   );
 };
--- src/containers/ShoppingCartContainer.js
+++ src/containers/ShoppingCartContainer.js
@@ -1,38 +1,63 @@
 import React from "react";
 import { connect } from "react-redux";
 import CartItem from "../components/CartItem";
+import { removeFromCart } from "../actions/index";
 
-const CartContainer = ({ myCart, products }) => {
-  console.log(myCart, products);
+const CartContainer = ({ myCart, products, removeFromCart }) => {
+  function handleDelete(e, id) {
+    removeFromCart(id);
+  }
+  let totalPrice = 0;
+  myCart.forEach((product) => {
+    if (products.find((item) => item.id === product.productId)) {
+      let productData = products.find((item) => item.id === product.productId);
+      totalPrice += productData.price * product.qtty;
+    }
+  });
   return (
     <section className="Shopping_cart wrapper">
       <h2>Mi carrito</h2>
-      <table>
-        <thead>
-          <tr>
-            <th />
-            <th>Producto</th>
-            <th>Cantidad</th>
-            <th>Precio</th>
-          </tr>
-        </thead>
-        <tbody>
-          {myCart.map((product) => (
-            <CartItem
-              qtty={product.qtty}
-              {...products.find((item) => item.id === product.productId)}
-            />
-          ))}
-        </tbody>
-      </table>
+      {myCart.length === 0 && <p>Aún no tienes productos en tu carrito</p>}
+      {Object.keys(myCart).length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th />
+              <th>Producto</th>
+              <th>Cantidad</th>
+              <th>Precio</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {myCart.map((product) => (
+              <CartItem
+                qtty={product.qtty}
+                handleDelete={handleDelete}
+                {...products.find((item) => item.id === product.productId)}
+              />
+            ))}
+            <tr key="totalProducts">
+              <td />
+              <td />
+              <td>Total: </td>
+              <td>{totalPrice}</td>
+              <td />
+            </tr>
+          </tbody>
+        </table>
+      )}
     </section>
   );
 };
 
 const mapStateToProps = (state) => ({
   ...state,
+  removeFromCart,
 });
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = {
+  removeFromCart,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartContainer);
